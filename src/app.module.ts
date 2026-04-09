bc5154eabf85f3158bd352e0f8f2fc72525cbebc
lafiriadh <lafiriadh35@gmail.com>
2026-04-09 17:38:40 +0100

    Adding states module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StudentSchema} from '../Schemas/states.schema';
+import { StatesModule } from './states/states.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'states'}),
+  imports: [ MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'states'}), StatesModule,
 ],
   controllers: [AppController],
   providers: [AppService],
